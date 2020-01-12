eea24590f7a65aa1d97b0de785996d97df6c45d3
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2020-01-12 09:45:39 -0800

    Use new overlays with `pkgsCross` (#10)

--- nix/default.nix
+++ nix/default.nix
@@ -1,7 +1,11 @@
 { pkgsPath ? <nixpkgs> }:
 
 let
-  pkgs = import pkgsPath {};
+  overlays = builtins.fetchTarball {
+    url = https://github.com/anmonteiro/nix-overlays/archive/5f01eb1.tar.gz;
+    sha256 = "16yp2y2qxpxx5wwyj9n1ihzyncj621mdmy0g6l33fpnz9ag8qh21";
+  };
+  pkgs = import pkgsPath { overlays = [ (import overlays) ]; };
 
   gitignoreSrc = pkgs.fetchFromGitHub {
     owner = "hercules-ci";
@@ -10,36 +14,16 @@ let
     sha256 = "1zd1ylgkndbb5szji32ivfhwh04mr1sbgrnvbrqpmfb67g2g3r9i";
   };
   inherit (import gitignoreSrc { inherit (pkgs) lib; }) gitignoreSource;
-
-  overlays = builtins.fetchTarball {
-    url = https://github.com/anmonteiro/nix-overlays/archive/23539c4.tar.gz;
-    sha256 = "1y1bip3wnvj92mcxniy71ajyb48r5navw09cph37i4dzg4vf8zid";
-  };
-
-  ocamlPackages = pkgs.ocaml-ng.ocamlPackages_4_09.overrideScope'
-    (pkgs.callPackage "${overlays}/ocaml" {});
+  pkgsCross = pkgs.pkgsCross.musl64.pkgsStatic;
 in
   {
     native = pkgs.callPackage ./generic.nix {
-      inherit ocamlPackages gitignoreSource;
+      inherit gitignoreSource;
     };
 
-    musl64 =
-      let pkgs = import "${overlays}/static.nix" {
-        inherit pkgsPath;
-        ocamlVersion = "4_09";
-      };
-      zlib = pkgs.zlib.override {
-        static = true;
-        shared = false;
-        splitStaticOutput = false;
-      };
-      in
-      pkgs.callPackage ./generic.nix {
-        static = true;
-        inherit gitignoreSource;
-        ocamlPackages = pkgs.ocaml-ng.ocamlPackages_4_09.overrideScope' (oself: osuper: {
-          camlzip = osuper.camlzip.override { inherit zlib; };
-        });
-      };
+    musl64 = pkgsCross.callPackage ./generic.nix {
+      static = true;
+      inherit gitignoreSource;
+      ocamlPackages = pkgsCross.ocaml-ng.ocamlPackages_4_09;
+    };
   }
