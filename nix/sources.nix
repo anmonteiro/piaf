006aa22507ea251903242be5a8fdd5e3fc4c0b25
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2020-02-19 20:26:36 -0800

    Fix static compilation for real

--- nix/sources.nix
+++ nix/sources.nix
@@ -2,8 +2,8 @@
 
 let
   overlays = builtins.fetchTarball {
-    url = https://github.com/anmonteiro/nix-overlays/archive/9cc86f6.tar.gz;
-    sha256 = "05zbm2x3b8zdkp1855s580r1la7wh5z7bya3x69gjyk1a1xakf6h";
+    url = https://github.com/anmonteiro/nix-overlays/archive/405d03b.tar.gz;
+    sha256 = "06pp8sgnqk061j0d43a5d7fycdbczc015nbabyi9qkw56zya6yqa";
   };
   pkgs = import <nixpkgs> {
     overlays = [
