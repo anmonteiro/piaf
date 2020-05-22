a244ecf9ae5abc793927b291863db731e611b38a
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2020-05-22 11:28:20 -0700

    cr

--- lib/status.ml
+++ lib/status.ml
@@ -1,5 +1,5 @@
 (*----------------------------------------------------------------------------
- * Copyright (c) 2019-2020, António Nuno Monteiro
+ * Copyright (c) 2020, António Nuno Monteiro
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
