39121a0eb84a14d5c7a28b8e6573f6f5a15649aa
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2020-07-25 18:48:09 -0700

    really fix

--- nix/sources.nix
+++ nix/sources.nix
@@ -3,7 +3,7 @@
 let
   overlays =
     builtins.fetchTarball
-      https://github.com/anmonteiro/nix-overlays/archive/e2141e0.tar.gz;
+      https://github.com/anmonteiro/nix-overlays/archive/c275496d.tar.gz;
 
 in
 
