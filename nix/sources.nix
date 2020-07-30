7775b1d51ec363241cdae652a1b0bdfaec948cf7
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2020-07-30 11:32:36 -0700

    Body: don't perform a copy when reading a body (#68)
    we're now using implementations handle either:
    
    - flow-control (h2), and can effectively tell the peer to stop
      sending us DATA frames if we haven't scheduled reads (consumed the
      body stream in Piaf's case).
    - backpressure (http/af), which issues Yield operations to the
      underlying runtime if we're not reading from the body.

--- nix/sources.nix
+++ nix/sources.nix
@@ -3,7 +3,7 @@
 let
   overlays =
     builtins.fetchTarball
-      https://github.com/anmonteiro/nix-overlays/archive/8937afc.tar.gz;
+      https://github.com/anmonteiro/nix-overlays/archive/8cefb11.tar.gz;
 
 in
 
