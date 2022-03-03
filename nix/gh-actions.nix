69efbf640f47519c230ac7c81ab1226ca8cdf7ea
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2022-03-03 13:43:36 -0800

    reuse

--- nix/gh-actions.nix
+++ nix/gh-actions.nix
@@ -1,5 +1,41 @@
 { lib }:
 
+let
+  commonSteps = [
+    {
+      "uses" = "actions/checkout@v2";
+      "with" = {
+        "submodules" = "recursive";
+      };
+    }
+    {
+      "uses" = "cachix/install-nix-action@v14.1";
+    }
+    {
+      "uses" = "cachix/cachix-action@v10";
+      "with" = {
+        "name" = "anmonteiro";
+        "signingKey" = "\${{ secrets.CACHIX_SIGNING_KEY }}";
+      };
+    }
+
+  ];
+
+  job = { steps, ... }@attrs: attrs // {
+    "strategy" = {
+      "fail-fast" = false;
+      "matrix" = {
+        "ocamlVersion" = [
+          "4_12"
+          "4_13"
+        ];
+      };
+    };
+    "steps" = commonSteps ++ steps;
+
+  };
+in
+
 lib.generators.toYAML { }
 {
   name = "Build";
@@ -12,69 +48,18 @@ lib.generators.toYAML { }
     };
   };
   "jobs" = {
-    "macOS" = {
+    "macOS" = job {
       "runs-on" = "macos-latest";
-      "strategy" = {
-        "fail-fast" = false;
-        "matrix" = {
-          "ocamlVersion" = [
-            "4_12"
-            "4_13"
-          ];
-        };
-      };
-      "steps" = [
-        {
-          "uses" = "actions/checkout@v2";
-          "with" = {
-            "submodules" = "recursive";
-          };
-        }
-        {
-          "uses" = "cachix/install-nix-action@v14.1";
-        }
-        {
-          "uses" = "cachix/cachix-action@v10";
-          "with" = {
-            "name" = "anmonteiro";
-            "signingKey" = "\${{ secrets.CACHIX_SIGNING_KEY }}";
-          };
-        }
+      steps = [
         {
           "name" = "Run nix-build";
           "run" = "nix-build ./nix/ci/test.nix -A native --argstr ocamlVersion \${{ matrix.ocamlVersion }}";
         }
       ];
     };
-    "linux" = {
+    "linux" = job {
       "runs-on" = "ubuntu-latest";
-      "strategy" = {
-        "fail-fast" = false;
-        "matrix" = {
-          "ocamlVersion" = [
-            "4_11"
-            "4_12"
-            "4_13"
-          ];
-        };
-      };
       "steps" = [
-        {
-          "uses" = "actions/checkout@v2";
-          "with" = {
-            "submodules" = "recursive";
-          };
-        }
-        {
-          "uses" = "cachix/install-nix-action@v14.1";
-        }
-        {
-          "uses" = "cachix/cachix-action@v10";
-          "with" = {
-            "name" = "anmonteiro";
-            "signingKey" = "\${{ secrets.CACHIX_SIGNING_KEY }}";
-          };
-        }
         {
           "name" = "Run nix-build";
           "run" = "nix-build ./nix/ci/test.nix -A native -A musl64 --argstr ocamlVersion \${{ matrix.ocamlVersion }}";
