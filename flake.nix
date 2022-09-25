ad8a760c8709a546ac5d9023e9cae96e9724858a
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2022-09-24 21:31:33 -0700

    flake

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs.nix-filter.url = "github:numtide/nix-filter";
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.nixpkgs.inputs.flake-utils.follows = "flake-utils";
-  inputs.nixpkgs.url = "github:anmonteiro/nix-overlays?rev=f1117857121c16e0dd44761a0c5cd3f6fa43f807";
+  inputs.nixpkgs.url = "github:anmonteiro/nix-overlays?rev=a4dafddea1c4cf3a49784fbcd73c7740912ee1fa";
 
   outputs = { self, nixpkgs, flake-utils, nix-filter }:
     flake-utils.lib.eachDefaultSystem (system:
