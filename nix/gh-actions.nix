97e2c678de47cb69f2b6199c81a9ec2231fa6f43
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2022-12-06 20:40:08 -0800

    5_00 -> 5_0

--- nix/gh-actions.nix
+++ nix/gh-actions.nix
@@ -22,7 +22,7 @@ let
   job =
     { steps
     , ocamlVersions ? [
-        "5_00"
+        "5_0"
       ]
     , ...
     }@attrs: (builtins.removeAttrs attrs [ "ocamlVersions" ]) // {
@@ -65,11 +65,11 @@ gh-actions.cachedBuild {
   os = {
     macos-latest = {
       name = "Run nix-build";
-      ocamlVersions = [ "5_00" ];
+      ocamlVersions = [ "5_0" ];
       run = "nix-build ./nix/ci/test.nix -A native --argstr ocamlVersion \${{ matrix.ocamlVersion }}";
     };
     ubuntu-latest = {
-      ocamlVersions = [ "5_00" ];
+      ocamlVersions = [ "5_0" ];
       name = "Run nix-build";
       run = "nix-build ./nix/ci/test.nix -A native -A musl64 --argstr ocamlVersion \${{ matrix.ocamlVersion }}";
     };
