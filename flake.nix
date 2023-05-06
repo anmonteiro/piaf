0372218fbae82526035c6b62fe0a70969ae759f7
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2023-05-05 18:55:10 -0700

    flake lock

--- flake.nix
+++ flake.nix
@@ -3,8 +3,10 @@
 
   inputs.nix-filter.url = "github:numtide/nix-filter";
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.nixpkgs.inputs.flake-utils.follows = "flake-utils";
-  inputs.nixpkgs.url = "github:nix-ocaml/nix-overlays";
+  inputs.nixpkgs = {
+    url = "github:nix-ocaml/nix-overlays";
+    inputs.flake-utils.follows = "flake-utils";
+  };
 
   outputs = { self, nixpkgs, flake-utils, nix-filter }:
     flake-utils.lib.eachDefaultSystem (system:
