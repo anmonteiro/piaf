48f0cd42379d6f31bc74d83c8af1eb4e6db2f765
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2023-07-10 21:09:49 -0700

    wip

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs.nix-filter.url = "github:numtide/nix-filter";
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.nixpkgs = {
-    url = "github:anmonteiro/nix-overlays";
+    url = "github:nix-ocaml/nix-overlays";
     inputs.flake-utils.follows = "flake-utils";
   };
 
