82275589952752433391475723a606f5a0b5868c
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2024-06-24 22:20:08 -0700

    revert some changes

--- lib/config.ml
+++ lib/config.ml
@@ -109,8 +109,8 @@ let default =
 
 let to_http1_config { body_buffer_size; buffer_size; _ } =
   { Httpun.Config.read_buffer_size = buffer_size
-  ; request_body_buffer_size = body_buffer_size
   ; response_buffer_size = buffer_size
+  ; request_body_buffer_size = body_buffer_size
   ; response_body_buffer_size = body_buffer_size
   }
 
